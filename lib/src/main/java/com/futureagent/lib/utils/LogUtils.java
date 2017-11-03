04792b60197361feaced812c6396332b45449e5a
skywalker <skywalker@thecover.cn>
2017-11-03 11:48:07 +0800

    [master] change:test jitpack version

--- lib/src/main/java/com/futureagent/lib/utils/LogUtils.java
+++ lib/src/main/java/com/futureagent/lib/utils/LogUtils.java
@@ -10,8 +10,7 @@ public class LogUtils {
     private static String TAG = "Future-Agent";
     private static boolean DEBUG = true;// 是否需要打印bug，可以在application的onCreate函数里面初始化
 
-    private LogUtils()
-    {
+    private LogUtils() {
         /* cannot be instantiated */
         throw new UnsupportedOperationException("cannot be instantiated");
     }
@@ -25,75 +24,64 @@ public class LogUtils {
     }
 
     // 下面四个是默认tag的函数
-    public static void i(String msg)
-    {
+    public static void i(String msg) {
         if (DEBUG)
             Log.i(TAG, msg);
     }
 
-    public static void d(String msg)
-    {
+    public static void d(String msg) {
         if (DEBUG)
             Log.i(TAG, msg);
     }
 
-    public static void e(String msg)
-    {
+    public static void e(String msg) {
         if (DEBUG)
             Log.e(TAG, msg);
     }
 
-    public static void v(String msg)
-    {
+    public static void v(String msg) {
         if (DEBUG)
             Log.v(TAG, msg);
     }
 
     // 下面是传入自定义tag的函数
-    public static void i(String tag, String msg)
-    {
+    public static void i(String tag, String msg) {
         if (DEBUG) {
             Log.i(TAG, getLogMsg(tag, msg));
         }
     }
 
-    public static void d(String tag, String msg)
-    {
+    public static void d(String tag, String msg) {
         if (DEBUG) {
             Log.i(TAG, getLogMsg(tag, msg));
         }
     }
 
-    public static void w(String tag, String msg)
-    {
+    public static void w(String tag, String msg) {
         if (DEBUG) {
             Log.w(TAG, getLogMsg(tag, msg));
         }
     }
 
-    public static void w(String tag, String msg, Throwable e)
-    {
+    public static void w(String tag, String msg, Throwable e) {
         if (DEBUG) {
             Log.w(TAG, getLogMsg(tag, msg + e.getMessage()));
         }
     }
 
-    public static void e(String tag, String msg)
-    {
+    public static void e(String tag, String msg) {
         if (DEBUG) {
             Log.e(TAG, getLogMsg(tag, msg));
         }
     }
 
-    public static void e(String tag, String msg, Throwable e)
-    {
+    public static void e(String tag, String msg, Throwable e) {
         if (DEBUG) {
             Log.e(TAG, getLogMsg(tag, msg + e.getMessage()));
         }
     }
 
-    public static void v(String tag, String msg)
-    {
+    public static void v(String tag, String msg) {
         if (DEBUG) {
             Log.i(TAG, getLogMsg(tag, msg));
         }
